b10c2e3435201752eadf015cc2f37db3ed0530af
Krishna Panchal <krishnapanchal64.kp@gmail.com>
2025-05-10 14:13:38 +0530

    Update login.php
    remove local changes

--- app/Views/login.php
+++ app/Views/login.php
@@ -57,13 +57,5 @@
 </body>
 </html>
 
-    <!-- <h2>Login</h2>
-    <form action="/c4/auth/doLogin" method="post">
-        <label>Username:</label><br>
-        <input type="text" name="username"><br><br>
-        <label>Password:</label><br>
-        <input type="password" name="password"><br><br>
-        <button type="submit">Login</button>
-    </form> -->
 </body>
 </html>
